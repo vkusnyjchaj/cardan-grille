e46de90c2fad984bd7f1c0894ef6806932ed4cfb
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2024-07-23 19:22:50 +0700

    Remove test code

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,6 @@ import "normalize.css"
 import './main.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
-import { generate } from './utils/generator';
-import { encode } from './utils/encoder'
-
-const grille = generate(4);
-const tables = encode('123456789abcdefghi', grille);
-console.log(tables);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
