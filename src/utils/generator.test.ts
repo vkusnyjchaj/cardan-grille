e24940b005f98cb8f1c9fcfc42dcb535646db215
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2024-07-23 13:44:36 +0700

    Add generator test

--- src/utils/generator.test.ts
+++ src/utils/generator.test.ts
@@ -23,12 +23,20 @@ test('Generates correct grille with predefined seed of top left and bottom right
 
 test('Generates correct grille with predefined seed', () => {
   const size = 4;
-  const seed = [0, 1, 2, 3];
+  const seed1 = [0, 1, 2, 3];
+  const seed2 = [3, 2, 1, 0];
 
-  expect(testExports.generateGrille(size, seed)).toEqual([
+  expect(testExports.generateGrille(size, seed1)).toEqual([
     [true, false, false, false],
     [false, false, false, true],
     [false, true, false, true],
     [false, false, false, false],
+  ]);
+
+  expect(testExports.generateGrille(size, seed2)).toEqual([
+    [false, false, true, false],
+    [false, true, false, false],
+    [false, false, false, false],
+    [true, false, true, false],
   ]);
 });
