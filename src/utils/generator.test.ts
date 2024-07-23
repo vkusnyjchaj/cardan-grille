188b845ed7dc219275167e9f02f141617f3bd1a8
tastyteadev <30465640+tastyteadev@users.noreply.github.com>
2024-07-23 13:08:21 +0700

    Leave only valuable test

--- src/utils/generator.test.ts
+++ src/utils/generator.test.ts
@@ -1,16 +1,6 @@
 import { expect, test } from 'vitest';
 import { testExports } from './generator';
 
-test('Generates correct grille with predefined seed for size 2', () => {
-  const size = 2;
-  const seed = [0];
-
-  expect(testExports.generateGrille(size, seed)).toEqual([
-    [true, false],
-    [false, false],
-  ]);
-})
-
 test('Generates correct grille with predefined seed for size 4', () => {
   const size = 4;
   const seed = [0, 0, 0, 0];
@@ -22,19 +12,3 @@ test('Generates correct grille with predefined seed for size 4', () => {
     [false, false, false, false],
   ]);
 })
-
-/*test('Generates correct grille with predefined seed for size 8', () => {
-  const size = 8;
-  const seed = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-  expect(testExports.generateGrille(size, seed)).toEqual([
-    [true, true, true, true, false, false, false, false,],
-    [true, true, true, true, false, false, false, false,],
-    [true, true, true, true, false, false, false, false,],
-    [true, true, true, true, false, false, false, false,],
-    [false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false],
-  ]);
-})*/
